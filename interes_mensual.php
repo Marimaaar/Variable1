acc95e1dbc559371f6daf8866f5b0492307cca21
karolz123 <320karol.52@gmail.com>
2022-10-27 07:28:53 -0500

    segundo cambio

---
+++ interes_mensual.php
@@ -0,0 +1,15 @@
+<?php
+$C_D = $_GET ['C_D'];
+$tasa1 = $C_D +($C_D * 0.02);
+$tasa2 = $C_D +($C_D * 0.045);
+$tasa3 = $C_D +($C_D * 0.07);
+
+if ($C_D < 500){    
+        echo "Debe 2% de intereses y " .$tasa1 ." de capital";    
+}elseif($C_D >= 500 && $C_D < 1500){ 
+    echo "Debe 4.5% de intereses y " .$tasa2 ." de capital";
+}elseif($C_D >=1500){
+    echo "Debe 7% de intereses y " .$tasa3 ." de capital";
+}
+
+?>
