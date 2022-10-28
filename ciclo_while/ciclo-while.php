183f988a39b4dd24058c4122517371137880bdf9
karolz123 <320karol.52@gmail.com>
2022-10-28 07:31:22 -0500

    cambios3

---
+++ ciclo_while/ciclo-while.php
@@ -0,0 +1,27 @@
+<?php
+     /*$n1 = 1;
+     while ($n1<5)//en el paréntesis se pone la condición
+     {
+echo "Marimar ";
+        $n1 = $n1 +1;
+        $n1 + =1;
+        $n1 ++;
+     } 
+     while($n2<=100){
+          echo $n2. " ";
+          $n2 = $n2 +2;
+     }*/
+     $n1 = 1;
+     $n2 = 0;
+
+     
+     while ($n1 <=20){
+          echo $n1;
+          $n2 = $n2 + $n1;
+          $n1++;
+     }
+     echo "<br>" .$n2;
+
+
+
+?>
